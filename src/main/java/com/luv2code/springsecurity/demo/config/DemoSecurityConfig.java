0538ff75392dfbf60c0c2e20c52224b492a3448b
Bhavya Arora <barora@BELC02V624PG8WN.sea.corp.expecn.com>
2018-03-10 14:24:51 -0800

    first commit

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,4 +1,24 @@
 package com.luv2code.springsecurity.demo.config;
 
-public class DemoSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig
+        extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //ad our users for in memory authentication
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("Bhavya").password("Arora").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("Rishab").password("Jolly").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("Bhavishya").password("Mittal").roles("EMPLOYEE"));
+
+    }
+
 }
--- src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -1,4 +1,8 @@
 package com.luv2code.springsecurity.demo.config;
 
-public class SecurityWebApplicationInitializer {
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+
 }
